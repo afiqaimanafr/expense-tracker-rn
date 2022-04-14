dd92d001ed3a5fa4d2a38857e33717ddd196aca4
afiqaimanafr <aaafr98@gmail.com>
2022-04-14 15:25:29 +0800

    update: latest

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
