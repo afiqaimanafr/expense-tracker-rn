2fd4ac77ebdaa0b8942c51550bc8cbeb2782c95f
afiqaimanafr <aaafr98@gmail.com>
2022-04-14 15:04:30 +0800

    update: latest

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+function ExpensesOutput({ expenses }) {
+  return (
+    <View>
+      <View>
+        <Text>Last 7 Days</Text>
+        <Text>RM99.99</Text>
+      </View>
+    </View>
+  );
+}
+
+export default ExpensesOutput;
+
+const styles = StyleSheet.create({});
