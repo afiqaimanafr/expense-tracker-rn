51ecf7c2499e131e4d7e2820e23d9df18b09f000
afiqaimanafr <aaafr98@gmail.com>
2022-04-14 15:15:08 +0800

    update: latest

--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -3,12 +3,12 @@ import { View, Text } from "react-native";
 function ExpensesSummary({ expenses, periodName }) {
   const expensesSum = expenses.reduce((sum, expense) => {
     return sum + expense.amount;
-  });
+  }, 0);
 
   return (
     <View>
       <Text>{periodName}</Text>
-      <Text>RM99.99</Text>
+      <Text>RM{expensesSum.toFixed(2)}</Text>
     </View>
   );
 }
