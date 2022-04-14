a1cf8faf8b80e355db1550076671b70c136fda76
afiqaimanafr <aaafr98@gmail.com>
2022-04-14 15:20:33 +0800

    update: latest

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,6 +3,39 @@ import { View } from "react-native";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 259.99,
+    date: new Date("2022-04-14"),
+  },
+  {
+    id: "e2",
+    description: "Food",
+    amount: 7.99,
+    date: new Date("2022-04-15"),
+  },
+  {
+    id: "e3",
+    description: "Drink",
+    amount: 2.59,
+    date: new Date("2022-04-15"),
+  },
+  {
+    id: "e4",
+    description: "Smartphone",
+    amount: 3259.99,
+    date: new Date("2022-04-19"),
+  },
+  {
+    id: "e1",
+    description: "Laptop",
+    amount: 4759.99,
+    date: new Date("2022-04-19"),
+  },
+];
+
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View>
