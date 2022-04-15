ad5ad7b72b76238d48df871cb8434c3642e59b9d
afiqaimanafr <aaafr98@gmail.com>
2022-04-15 15:36:15 +0800

    update: latest

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -5,7 +5,10 @@ import { GlobalStyles } from "../../constants/styles";
 function Button({ children, onPress, mode, style }) {
   return (
     <View style={style}>
-      <Pressable onPress={onPress}>
+      <Pressable
+        onPress={onPress}
+        style={({ pressed }) => pressed && styles.pressed}
+      >
         <View style={[styles.button, mode === "flat" && styles.flat]}>
           <Text style={[styles.buttonText, mode === "flat" && styles.flatText]}>
             {children}
