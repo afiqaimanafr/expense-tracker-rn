992f4fdfb67f022b91cfea95a35a7ecb1d047d39
afiqaimanafr <aaafr98@gmail.com>
2022-05-17 16:22:57 +0800

    update: latest

--- util/http.js
+++ util/http.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-function storeExpense(expenseData) {
+export function storeExpense(expenseData) {
   axios.post(
     "https://react-native-expense-planner-default-rtdb.firebaseio.com/expenses.json",
     expenseData
   );
 }
-
-export default storeExpense;
