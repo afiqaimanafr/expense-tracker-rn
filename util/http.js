aa1f0d371e7b01d7fe361359df0707f0a78c865f
afiqaimanafr <aaafr98@gmail.com>
2022-05-19 12:29:32 +0800

    update: latest

--- util/http.js
+++ util/http.js
@@ -30,8 +30,8 @@ export async function fetchExpenses() {
   return expenses;
 }
 
-export async function updateExpense(id, expenseData) {
-  axios.put(BACKEND_URL + `/expenses/${id}.json`, expenseData);
+export function updateExpense(id, expenseData) {
+  return axios.put(BACKEND_URL + `/expenses/${id}.json`, expenseData);
 }
 
 export async function deleteExpense() {}
